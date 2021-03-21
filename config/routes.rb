a25be30f703fbdb1d0bbbf233edc16ac007e7d82
Jmwofford <wofford.codes@gmail.com>
2021-03-21 01:37:32 -0500

    initDeploy:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'authors#index'
+  get '/training' => 'authors#training'
   resources :authors
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
