ac7f7a2a1b4800ae2cbd364776ceea606b4990c1
Jmwofford <wofford.codes@gmail.com>
2021-03-21 21:01:35 -0500

    trainingPage:

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :authors
   resources :comments
   get '/' => 'authors#index'
+  get '/introspect' => 'authors#introspect'
   get '/training' => 'authors#training'
   get '/contact' => 'comments#index'
 end
