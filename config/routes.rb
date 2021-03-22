04a2e5888897d110beb8e905ba4ccbad0ee5e029
Jmwofford <wofford.codes@gmail.com>
2021-03-21 19:31:59 -0500

    trainingPage:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :comments
   get '/' => 'authors#index'
   get '/training' => 'authors#training'
+  get '/contact' => 'comments#index'
   resources :authors
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_004421) do
+ActiveRecord::Schema.define(version: 2021_03_21_222919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_03_21_004421) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
