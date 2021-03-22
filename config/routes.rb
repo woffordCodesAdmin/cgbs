e4851fedeae5f73bf3ae9dbe2d866a2a083dfeab
Jmwofford <wofford.codes@gmail.com>
2021-03-21 19:52:12 -0500

    trainingPage:

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  resources :authors
   resources :comments
   get '/' => 'authors#index'
   get '/training' => 'authors#training'
   get '/contact' => 'comments#index'
-  resources :authors
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
